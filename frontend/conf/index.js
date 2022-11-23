0c2ee3ab5da1d447ca836cf019f53264f390c180
CrioUser <crio-user@criodo.com>
2022-11-24 01:18:51 +0530

    ME_QTRIPDYNAMIC_MODULE_ADVENTURES

--- frontend/conf/index.js
+++ frontend/conf/index.js
@@ -1,4 +1,4 @@
 
-const config = { backendEndpoint: "http://15.207.22.201:8082" };
+const config = { backendEndpoint: "http://3.111.125.73:8082" };
 
 export default config;
--- frontend/modules/adventures_page.js
+++ frontend/modules/adventures_page.js
@@ -1,17 +1,40 @@
 
 import config from "../conf/index.js";
 
+// console.log(getCityFromURL("http://3.109.101.134:8081/pages/adventures/city?=bengaluru"))
+
+// console.log("Hii")
 //Implementation to extract city from query params
 function getCityFromURL(search) {
   // TODO: MODULE_ADVENTURES
   // 1. Extract the city id from the URL's Query Param and return it
+  // console.log(search)
+  const queryParam = new URLSearchParams(search)
+  const city = queryParam.get('city')
+  // return 
+  // console.log(val)
+
+  return city
 
 }
+
+
 
 //Implementation of fetch call with a paramterized input based on city
 async function fetchAdventures(city) {
   // TODO: MODULE_ADVENTURES
   // 1. Fetch adventures using the Backend API and return the data
+  const city_name = city
+  try{
+    const res = await fetch( `/adventures?city=${city_name}`).then(res => res.json())
+    return res
+  }catch(e){
+    return null
+  }
+
+
+
+
 
 }
 
@@ -19,6 +42,72 @@ async function fetchAdventures(city) {
 function addAdventureToDOM(adventures) {
   // TODO: MODULE_ADVENTURES
   // 1. Populate the Adventure Cards and insert those details into the DOM
+  adventures.forEach(adv => {
+    const id = adv.id
+    const category = adv.category
+    const imgURL = adv.image
+    const name = adv.name
+    const costPerHead = adv.costPerHead
+    const duration = adv.duration
+
+    const link = `detail?adventure=${id}`
+
+    // Target row element and append everything to row
+    let row = document.getElementById("data")
+
+    let cardDiv = document.createElement("div")
+    cardDiv.className = "col-lg-3 col-6 mb-4"
+
+    let a = document.createElement("a")
+    a.setAttribute("href",link)
+    a.setAttribute("id",id)
+
+    let tile = document.createElement("div")
+    tile.className = "tile border rounded activity-card"
+
+    // Card Img
+    let cardImg = document.createElement("img")
+    cardImg.setAttribute("src",imgURL)
+
+    // for Text elements
+    let tileText = document.createElement("div")
+    tileText.className = "tile-text"
+
+    // For category
+    let categ = document.createElement("p")
+    categ.className = "category-banner"
+    categ.textContent = category
+
+  
+
+    // for first 2 text elements -> Name and Price
+    let textDiv1 = document.createElement("div")
+    textDiv1.className = "d-flex justify-content-between"
+
+    let Name = name
+    let cost = costPerHead
+    textDiv1.append(Name,cost)
+
+
+    // for last 2 text elements -> Duration and time
+    let textDiv2 = document.createElement("div")
+    textDiv2.className = "d-flex justify-content-between"
+    let dur = document.createElement("p")
+    dur.textContent = "Duration"
+    textDiv2.append(dur,duration)
+
+
+    tileText.append(textDiv1,textDiv2)
+
+    tile.append(tileText,categ,cardImg)
+
+    a.append(tile)
+
+    cardDiv.append(a)
+
+    row.append(cardDiv)
+
+  })
 
 }
 
